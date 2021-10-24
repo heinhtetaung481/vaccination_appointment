687955d3fabdffb878d2179bba382b2c7821474b
Hein Aung <heinaung@Heins-MacBook-Pro.local>
2021-10-24 20:58:25 +0800

    remove config file from gitignore to deploy to heroku

---
+++ config.js
@@ -0,0 +1,7 @@
+const MONGODB = 'mongodb+srv://mgdb_usr:Hha196021@sandbox.mkqnh.mongodb.net/vaccination_appointments?retryWrites=true&w=majority'
+const PORT = 3001
+
+export {
+    MONGODB,
+    PORT
+}
