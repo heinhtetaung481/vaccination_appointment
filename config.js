5566294a6fc402fa7fa13a2b2eb91ac637cd35d8
heinhtetaung481 <heinhtetaung481@gmail.com>
2022-02-11 01:11:24 +0800

    delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const MONGODB = 'mongodb+srv://mgdb_usr:Hha196021@sandbox.mkqnh.mongodb.net/vaccination_appointments?retryWrites=true&w=majority'
-const PORT = 3001
-
-export {
-    MONGODB,
-    PORT
-}
